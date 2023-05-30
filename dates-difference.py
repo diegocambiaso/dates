fac918163ddabbd360331b521f6c0f03a885100e
Diego Cambiaso <diegocambiaso@gmail.com>
2023-05-29 20:03:04 -0500

    Create dates-difference.py

---
+++ dates-difference.py
@@ -0,0 +1,11 @@
+import datetime
+
+# Define the start and end dates
+start_date = datetime.datetime(2023, 5, 29, 17, 59, 35)
+end_date = datetime.datetime(2023, 6, 1, 12, 0, 0)
+
+# Calculate the time difference
+time_difference = end_date - start_date
+
+# Print the time difference
+print(time_difference)
